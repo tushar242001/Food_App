0763f7828aeb8767c95267618084e4d24293205a
tushar242001 <tusharadling24@gmail.com>
2024-03-04 13:11:57 +0530

    Changes Done in Files

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Home from './screens/Home';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
-   <div><Home></Home></div>
+    <Router>
+    <div>
+      <Routes>
+        <Route exact path="/"element={<Home/>}/>
+        <Route exact path="/login"element={<Login/>}/>
+      </Routes>
+    </div>
+    </Router>
+   
   );
 }
 
