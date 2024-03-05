0b204a039f7ad6c786088ee09aa3a4813a420ee6
tushar242001 <tusharadling24@gmail.com>
2024-03-06 00:08:02 +0530

    initial changes

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
+import Carousal from "../components/Carousal";
 
 export default function Home() {
   return (
     <div>
       <div>
         <Navbar />
+      </div>
+      <div>
+        <Carousal/>
       </div>
       <div>
         <Card/>
